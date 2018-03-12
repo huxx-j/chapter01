27ac853d284adb7565bab1492aa41c8fce0cf25e
Huxx-j(장성우) <perfekta213@gmail.com>
2018-03-12 16:10:09 +0900

    1

---
+++ src/com/javaex/ex05/ArrayEx01.java
@@ -0,0 +1,15 @@
+package com.javaex.ex05;
+
+public class ArrayEx01 {
+    public static void main(String[] args) {
+        char[] c = new char[3];
+
+        c[0] = 'B';
+        c[1] = 'I';
+        c[2] = 'T';
+
+        System.out.println("c[0] : " + c[0]);
+        System.out.println("c[1] : " + c[1]);
+        System.out.println("c[2] : " + c[2]);
+    }
+}
