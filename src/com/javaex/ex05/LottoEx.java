07b0fa0a9aaa6a908cb9df72b5a27d66034f139e
Huxx-j(장성우) <perfekta213@gmail.com>
2018-03-12 16:41:09 +0900

    1

---
+++ src/com/javaex/ex05/LottoEx.java
@@ -0,0 +1,12 @@
+package com.javaex.ex05;
+
+public class LottoEx {
+    public static void main(String[] args) {
+        int lotto[] = new int[6];
+
+        for (int i = 0; i < lotto.length; i++) {
+            lotto[i] = (int)(Math.random()*45) + 1;
+            System.out.print(lotto[i] + "\t");
+        }
+    }
+}
